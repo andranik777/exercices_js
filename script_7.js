b68d94c8c4da75afc3751b25f1f423dc242ea9f9
andranik777 <andranik.hakabyan42@gmail.com>
2020-11-12 21:24:00 +0100

    y second commit

--- script_7.js
+++ script_7.js
@@ -1,10 +1,22 @@
-var a = "tu vas bien ?"
+var a = prompt("Pose une question au gentill chien chien")
+console.log(a)
 
-var regex = /"?"/gm;
-var found = a.match(regex);
+var regex = /[?]/g;
+var maching = a.match(regex);
 
 
+if (maching == "?") {
+    console.log("Ouais Ouais...")
+} else if (a.match(/Fortnite/gi) == "Fortnite" || a.match(/Fortnite/gi) == "fortnite") {
+    console.log("on s'fait une partie soum-soum")
+} else if (a === "") {
+    console.log("t'es en PLS ?")
+} else if (a === a.toUpperCase()) {
+    console.log("Pwa, calme - toi...")
 
+} else {
+    console.log("balek.")
+}
 
 
 
@@ -16,3 +28,4 @@ var found = a.match(regex);
 
 
 // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/match
+// https://www.w3schools.com/jsref/jsref_regexp_charset.asp
